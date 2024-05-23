034b57ec39bca9d9021c5b404100d2900bb40b6c
uhyo <uhyo@uhy.ooo>
2024-05-24 00:25:46 +0900

    form action

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import React, {
   useState,
   useTransition,
 } from "react";
+import { useFormStatus } from "react-dom";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { LikeData, getLikes, likePost } from "./likes";
@@ -22,15 +23,33 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <ActionState />
       </Suspense>
+      <h2>useOptimistic + form action</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormAction />
+      </Suspense>
+      <h2>useOptimistic + form action + useFormStatus</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormActionStatus />
+      </Suspense>
     </>
   );
 }
 
-const Post: React.FC<{
-  likes: LikeData;
-  isPending: boolean;
-  like: () => void;
-}> = ({ likes: { liked, likes }, isPending, like }) => {
+const Post: React.FC<
+  {
+    likes: LikeData;
+    isPending: boolean;
+  } & (
+    | {
+        buttonType: "submit";
+        like?: undefined;
+      }
+    | {
+        buttonType: "button";
+        like: () => void;
+      }
+  )
+> = ({ likes: { liked, likes }, isPending, like, buttonType }) => {
   return (
     <div className="card">
       <a href="https://react.dev" target="_blank">
@@ -39,10 +58,11 @@ const Post: React.FC<{
       <div>
         <span>何か良い感じの投稿</span>
         <button
+          type={buttonType}
           className="likes"
           data-liked={liked}
           data-pending={isPending}
-          onClick={() => like()}
+          onClick={buttonType === "button" ? like : undefined}
         >
           ❤ {likes}
         </button>
@@ -68,15 +88,25 @@ const Optimistic: React.FC = () => {
     });
   };
 
-  return <Post likes={displayData} isPending={isPending} like={like} />;
+  return (
+    <Post
+      likes={displayData}
+      isPending={isPending}
+      buttonType="button"
+      like={like}
+    />
+  );
 };
 
 const ActionState: React.FC = () => {
-  const [serverData, runAction, isPending] = useActionState(async () => {
-    addOptimisticLike(!displayData.liked);
-    const result = await likePost();
-    return result;
-  }, use(getLikes()));
+  const [serverData, runAction, isPending] = useActionState(
+    async (currentState) => {
+      addOptimisticLike(!currentState.liked);
+      const result = await likePost();
+      return result;
+    },
+    use(getLikes())
+  );
   const [displayData, addOptimisticLike] = useOptimistic(
     serverData,
     (state, nextLiked: boolean) => {
@@ -86,7 +116,61 @@ const ActionState: React.FC = () => {
   const like = () => {
     runAction();
   };
-  return <Post likes={displayData} isPending={isPending} like={like} />;
+  return (
+    <Post
+      likes={displayData}
+      isPending={isPending}
+      buttonType="button"
+      like={like}
+    />
+  );
+};
+
+const FormAction: React.FC = () => {
+  const [serverData, setLikes] = useState(use(getLikes()));
+  const [displayData, addOptimisticLike] = useOptimistic(
+    serverData,
+    (state, nextLiked: boolean) => {
+      return { liked: nextLiked, likes: state.likes + (nextLiked ? 1 : -1) };
+    }
+  );
+  const like = async () => {
+    addOptimisticLike(!displayData.liked);
+    const newData = await likePost();
+    setLikes(newData);
+  };
+
+  return (
+    <form action={like}>
+      <Post likes={displayData} isPending={false} buttonType="submit" />
+    </form>
+  );
+};
+
+const FormActionStatus: React.FC = () => {
+  const [serverData, setLikes] = useState(use(getLikes()));
+  const [displayData, addOptimisticLike] = useOptimistic(
+    serverData,
+    (state, nextLiked: boolean) => {
+      return { liked: nextLiked, likes: state.likes + (nextLiked ? 1 : -1) };
+    }
+  );
+  const like = async () => {
+    addOptimisticLike(!displayData.liked);
+    const newData = await likePost();
+    setLikes(newData);
+  };
+
+  return (
+    <form action={like}>
+      <FormActionStatusInner likes={displayData} />
+    </form>
+  );
+};
+
+const FormActionStatusInner: React.FC<{ likes: LikeData }> = ({ likes }) => {
+  const { pending } = useFormStatus();
+  return <Post likes={likes} buttonType="submit" isPending={pending} />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 /// <reference types="react/canary" />
+/// <reference types="react-dom/canary" />
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
