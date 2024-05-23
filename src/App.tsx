7961a6ca33cbdcae498c447537080a16324d8089
uhyo <uhyo@uhy.ooo>
2024-05-23 23:28:53 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,25 @@ import React, {
 } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { getLikes, likePost } from "./likes";
+import { LikeData, getLikes, likePost } from "./likes";
 
 function App() {
   return (
     <>
       <h1>React 19 Beta playground</h1>
+      <h2>useOptimistic + useTransition</h2>
       <Suspense fallback={<div>Loading...</div>}>
-        <Post />
+        <Optimistic />
       </Suspense>
     </>
   );
 }
 
-const Post: React.FC = () => {
-  const [serverData, setLikes] = useState(use(getLikes()));
-  const [{ liked, likes }, addOptimisticLike] = useOptimistic(
-    serverData,
-    (state, nextLiked: boolean) => {
-      return { liked: nextLiked, likes: state.likes + (nextLiked ? 1 : -1) };
-    }
-  );
-  const [isPending, startTransition] = useTransition();
-  const like = () => {
-    startTransition(async () => {
-      addOptimisticLike(!liked);
-      const newData = await likePost();
-      setLikes(newData);
-    });
-  };
-
+const Post: React.FC<{
+  likes: LikeData;
+  isPending: boolean;
+  like: () => void;
+}> = ({ likes: { liked, likes }, isPending, like }) => {
   return (
     <div className="card">
       <a href="https://react.dev" target="_blank">
@@ -55,6 +44,26 @@ const Post: React.FC = () => {
       </div>
     </div>
   );
+};
+
+const Optimistic: React.FC = () => {
+  const [serverData, setLikes] = useState(use(getLikes()));
+  const [displayData, addOptimisticLike] = useOptimistic(
+    serverData,
+    (state, nextLiked: boolean) => {
+      return { liked: nextLiked, likes: state.likes + (nextLiked ? 1 : -1) };
+    }
+  );
+  const [isPending, startTransition] = useTransition();
+  const like = () => {
+    startTransition(async () => {
+      addOptimisticLike(!displayData.liked);
+      const newData = await likePost();
+      setLikes(newData);
+    });
+  };
+
+  return <Post likes={displayData} isPending={isPending} like={like} />;
 };
 
 export default App;
